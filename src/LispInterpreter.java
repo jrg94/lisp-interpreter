552d98de76a18a3c222eece297f4748d27a5a84d
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-01 21:24:17 -0400

    Implemented a basic lisp tokenizer

--- src/LispInterpreter.java
+++ src/LispInterpreter.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class LispInterpreter {
     public static void main(String[] args) {
         // TODO: get Lisp code from user
@@ -5,5 +7,9 @@ public class LispInterpreter {
         // TODO: generate abstract syntax tree from code
         // TODO: evaluate AST
         // TODO: report errors
+        String someLispCode = "(2 3)";
+        String lispPlusWhitespace = someLispCode.replaceAll("\\(", " ( ").replaceAll("\\)", " ) ").trim();
+        String[] lispTokens = lispPlusWhitespace.split("\\s+");
+        System.out.println(Arrays.toString(lispTokens));
     }
 }
