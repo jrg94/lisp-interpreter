4249573182be4d6299ef3d18b460708b76c0ffc0
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-01 21:16:04 -0400

    Setup the interpreter file for execution

---
+++ src/LispInterpreter.java
@@ -0,0 +1,9 @@
+public class LispInterpreter {
+    public static void main(String[] args) {
+        // TODO: get Lisp code from user
+        // TODO: tokenize Lisp code
+        // TODO: generate abstract syntax tree from code
+        // TODO: evaluate AST
+        // TODO: report errors
+    }
+}
