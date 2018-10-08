cd2d74d5d7f991771d490f8158544e21910e3840
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-08 17:17:49 -0400

    Implemented IntegerAtom equals

--- src/IntegerAtom.java
+++ src/IntegerAtom.java
@@ -9,6 +9,20 @@ public class IntegerAtom implements SExpression {
         return value;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        boolean isEqual;
+        if (o == null) {
+            isEqual = false;
+        } else if (o instanceof IntegerAtom) {
+            IntegerAtom atom = (IntegerAtom) o;
+            isEqual = this.getValue() == atom.getValue();
+        } else {
+            isEqual = false;
+        }
+        return isEqual;
+    }
+
     public String toString() {
         return Integer.toString(value);
     }
