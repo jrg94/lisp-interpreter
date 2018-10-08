11e82af6a6c9294a57a32ef4a86d598d11e012f3
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-07 23:16:05 -0400

    Implemented symbol table

--- src/LispInterpreter.java
+++ src/LispInterpreter.java
@@ -7,6 +7,18 @@ import java.util.Stack;
 public class LispInterpreter {
 
     public static final SymbolicAtom NIL = new SymbolicAtom("NIL");
+    public static final SymbolicAtom T = new SymbolicAtom("T");
+
+    public ArrayList<SymbolicAtom> dList;
+    public ArrayList<SymbolicAtom> symbols;
+
+    public LispInterpreter() {
+        dList = new ArrayList<SymbolicAtom>();
+        dList.add(new SymbolicAtom("cons"));
+        dList.add(new SymbolicAtom("car"));
+        dList.add(new SymbolicAtom("cdr"));
+        symbols = new ArrayList<SymbolicAtom>();
+    }
 
     /**
      * Converts a lisp program to a stack of tokens.
@@ -66,6 +78,13 @@ public class LispInterpreter {
         return curr;
     }
 
+    /**
+     * A dot notation parsing function.
+     * 
+     * @param exps a list of expressions for a given SExpression
+     * @return an SExpression from the list of SExpressions
+     * @throws LispSyntaxException when the list of expressions is not 2
+     */
     private SExpression parseDotNotation(ArrayList<SExpression> exps) throws LispSyntaxException {
         NonAtom curr;
         if (exps.size() != 2) {
@@ -82,6 +101,7 @@ public class LispInterpreter {
      * A list notation parsing function.
      * 
      * @param exps a list of expressions for a given SExpression
+     * @return an SExpression from the list of SExpressions
      */
     private SExpression parseListNotation(ArrayList<SExpression> exps) {
         SExpression curr;
@@ -108,16 +128,37 @@ public class LispInterpreter {
      * 
      * @param token a token
      * @return the token as an s expression
+     * @throws LispSyntaxException
      */
-    public SExpression createAtom(String token) {
+    public SExpression createAtom(String token) throws LispSyntaxException {
         SExpression atom;
         try {
             int num = Integer.parseInt(token);
             atom = new IntegerAtom(num);
         } catch (NumberFormatException e) {
-            atom = new SymbolicAtom(token);
+            atom = searchSymbols(token);
         }
         return atom;
+    }
+
+    /**
+     * A helper method for searching the symbol table. If a symbol is found,
+     * that symbol is returned. Otherwise, a new symbol is created, added to the
+     * symbol table, and returned.
+     * 
+     * @param token a token
+     * @return an existing symbolic atom from the symbol table; a new symbolic
+     *         atom otherwise
+     */
+    private SymbolicAtom searchSymbols(String token) {
+        for (SymbolicAtom symbol : symbols) {
+            if (symbol.getValue().equals(token)) {
+                return symbol;
+            }
+        }
+        SymbolicAtom symbol = new SymbolicAtom(token);
+        symbols.add(symbol);
+        return symbol;
     }
 
     public SExpression read(String lispProgram) throws LispSyntaxException {
@@ -138,9 +179,6 @@ public class LispInterpreter {
     }
 
     public static void main(String[] args) {
-        // TODO: generate abstract syntax tree from code
-        // TODO: evaluate AST
-        // TODO: report errors
         LispInterpreter lisp = new LispInterpreter();
         System.out.println("Welcome to the CSE6341 Lisp Interpreter by Jeremy Grifski!");
         Scanner in = new Scanner(System.in);
