b7d564d0c3da8cde276a3214c332e6daa80c88ca
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-15 00:42:26 -0400

    Updated version

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ ! -e lisp-interpreter.jar ]; then
-	wget https://github.com/jrg94/lisp-interpreter/releases/download/v1.0.0/lisp-interpreter.jar
+	wget https://github.com/jrg94/lisp-interpreter/releases/download/v1.1.0/lisp-interpreter.jar
 fi
 
 java -jar lisp-interpreter.jar
