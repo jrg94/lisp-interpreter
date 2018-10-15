cbc6ebcc8543b350977e0397ecf7adfa6889d1aa
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-15 00:37:31 -0400

    Created a shell script

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if [ ! -f lisp-interpreter.jar ]; then
+	wget https://github.com/jrg94/lisp-interpreter/releases/download/v1.0.0/lisp-interpreter.jar
+fi
+
+java -jar lisp-interpreter.jar
