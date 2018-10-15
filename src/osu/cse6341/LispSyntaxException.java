64fa582cfa0cb665dae9e7f952cfad8b67626b34
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-14 23:34:04 -0400

    Added comments to custom exception

--- src/osu/cse6341/LispSyntaxException.java
+++ src/osu/cse6341/LispSyntaxException.java
@@ -1,26 +1,59 @@
 package osu.cse6341;
+
+/**
+ * The Lisp Syntax Exception class which extends the generic Exception class.
+ * 
+ * @author Jeremy Grifski
+ */
 public class LispSyntaxException extends Exception {
 
     private static final long serialVersionUID = -4819106933558488363L;
 
+    /**
+     * The base constructor.
+     */
     public LispSyntaxException() {
         super();
     }
 
-    public LispSyntaxException(String arg0) {
-        super(arg0);
+    /**
+     * The message constructor.
+     * 
+     * @param msg the error message
+     */
+    public LispSyntaxException(String msg) {
+        super(msg);
     }
 
-    public LispSyntaxException(Throwable arg0) {
-        super(arg0);
+    /**
+     * The throwable constructor
+     * 
+     * @param err the error
+     */
+    public LispSyntaxException(Throwable err) {
+        super(err);
     }
 
-    public LispSyntaxException(String arg0, Throwable arg1) {
-        super(arg0, arg1);
+    /**
+     * The message and throwable constructor.
+     * 
+     * @param msg the error message
+     * @param err the error
+     */
+    public LispSyntaxException(String msg, Throwable err) {
+        super(msg, err);
     }
 
-    public LispSyntaxException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
-        super(arg0, arg1, arg2, arg3);
+    /**
+     * The complete constructor.
+     * 
+     * @param msg the error message
+     * @param err the error
+     * @param suppression the suppression enable bit
+     * @param stackTrace the stackTrace enable bit
+     */
+    public LispSyntaxException(String msg, Throwable err, boolean suppression, boolean stackTrace) {
+        super(msg, err, suppression, stackTrace);
     }
 
 }
