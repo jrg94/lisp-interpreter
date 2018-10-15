4a11c98cb7fa84d088ba5fda1e87f265077069dc
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-14 23:27:10 -0400

    Added comments to the NonAtom class

--- src/osu/cse6341/NonAtom.java
+++ src/osu/cse6341/NonAtom.java
@@ -1,24 +1,56 @@
 package osu.cse6341;
+
+/**
+ * The NonAtom class which is a type of s-expression.
+ * 
+ * @author Jeremy Grifski
+ */
 public class NonAtom implements SExpression {
+
     private SExpression left;
     private SExpression right;
 
+    /**
+     * Sets the left node of this binary tree.
+     * 
+     * @param left any s-expression
+     */
     public void setLeft(SExpression left) {
         this.left = left;
     }
 
+    /**
+     * Sets the right node of this binary tree.
+     * 
+     * @param right any s-expression
+     */
     public void setRight(SExpression right) {
         this.right = right;
     }
 
+    /**
+     * Gets the left node of this binary tree.
+     * 
+     * @return the left s-expression
+     */
     public SExpression getLeft() {
         return left;
     }
 
+    /**
+     * Gets the right node of this binary tree.
+     * 
+     * @return the right s-expression
+     */
     public SExpression getRight() {
         return right;
     }
 
+    /**
+     * The standard override of the equals method.
+     * 
+     * @return true if the two binary trees are equal
+     */
     @Override
     public boolean equals(Object o) {
         boolean isEqual;
@@ -33,6 +65,11 @@ public class NonAtom implements SExpression {
         return isEqual;
     }
 
+    /**
+     * The standard override of the toString method.
+     * 
+     * @return the binary tree as a string
+     */
     public String toString() {
         return "(" + left.toString() + " . " + right.toString() + ")";
     }
