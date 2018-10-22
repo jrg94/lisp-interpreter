5a539f89ad0f843475f11074440eaaa800afdc92
Jeremy Grifski <jeremy.grifski@gmail.com>
2018-10-21 22:44:53 -0400

    Created evaluation exception

---
+++ src/osu/cse6341/LispEvaluationException.java
@@ -0,0 +1,59 @@
+package osu.cse6341;
+
+/**
+ * The Lisp Evaluation Exception class which extends the generic Exception class.
+ *
+ * @author Jeremy Grifski
+ */
+public class LispEvaluationException extends Exception {
+
+    private static final long serialVersionUID = -4819106933558488363L;
+
+    /**
+     * The base constructor.
+     */
+    public LispSyntaxException() {
+        super();
+    }
+
+    /**
+     * The message constructor.
+     *
+     * @param msg the error message
+     */
+    public LispSyntaxException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * The throwable constructor
+     *
+     * @param err the error
+     */
+    public LispSyntaxException(Throwable err) {
+        super(err);
+    }
+
+    /**
+     * The message and throwable constructor.
+     *
+     * @param msg the error message
+     * @param err the error
+     */
+    public LispSyntaxException(String msg, Throwable err) {
+        super(msg, err);
+    }
+
+    /**
+     * The complete constructor.
+     *
+     * @param msg the error message
+     * @param err the error
+     * @param suppression the suppression enable bit
+     * @param stackTrace the stackTrace enable bit
+     */
+    public LispSyntaxException(String msg, Throwable err, boolean suppression, boolean stackTrace) {
+        super(msg, err, suppression, stackTrace);
+    }
+
+}
--- src/osu/cse6341/SExpression.java
+++ src/osu/cse6341/SExpression.java
@@ -2,9 +2,11 @@ package osu.cse6341;
 
 /**
  * The S-Expresion Interface.
- * 
+ *
  * @author Jeremy Grifski
  */
 public interface SExpression {
+
+  // TODO: Add evaluate function
 
 }
