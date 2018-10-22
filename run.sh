284963c1661e63d525038ddc866581db119dd68f
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-22 19:03:25 -0400

    Removed ant file

--- run.sh
+++ run.sh
@@ -2,3 +2,11 @@
 
 javac src/osu/cse6341/*.java
 java -cp src/ osu.cse6341.LispInterpreter
+
+# Uncomment if you'd prefer to use the jar
+# 
+# if [ ! -e lisp-interpreter.jar ]; then
+#	wget https://github.com/jrg94/lisp-interpreter/releases/download/v1.1.0/lisp-interpreter.jar
+# fi
+#
+# java -jar lisp-interpreter.jar
