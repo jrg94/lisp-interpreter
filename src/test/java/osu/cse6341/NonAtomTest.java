29a01674cef38a254aa4cebe855ed816d41e81cb
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-24 00:12:42 -0400

    Improved testing

--- src/test/java/osu/cse6341/NonAtomTest.java
+++ src/test/java/osu/cse6341/NonAtomTest.java
@@ -8,6 +8,55 @@ import org.junit.Test;
 
 public class NonAtomTest {
 
+    /**
+     * A helper method for building a lisp list of Symbolic Atoms.
+     * 
+     * @param symbols a list of string symbols
+     * @return the root of the list
+     */
+    private NonAtom buildList(String... symbols) {
+        NonAtom root = new NonAtom();
+        NonAtom curr = root;
+        for (int i = 0; i < symbols.length; i++) {
+            curr.setLeft(new SymbolicAtom(symbols[i]));
+            if (i + 1 == symbols.length) {
+                curr.setRight(SExpression.NIL);
+            } else {
+                curr.setRight(new NonAtom());
+                curr = (NonAtom) curr.getRight();
+            }
+        }
+        return root;
+    }
+
+    /**
+     * A helper method for building a list list of Integer Atoms.
+     * 
+     * @param symbols a list of integers
+     * @return the root of the list
+     */
+    private NonAtom buildList(int... symbols) {
+        NonAtom root = new NonAtom();
+        NonAtom curr = root;
+        for (int i = 0; i < symbols.length; i++) {
+            curr.setLeft(new IntegerAtom(symbols[i]));
+            if (i + 1 == symbols.length) {
+                curr.setRight(SExpression.NIL);
+            } else {
+                curr.setRight(new NonAtom());
+                curr = (NonAtom) curr.getRight();
+            }
+        }
+        return root;
+    }
+
+    private NonAtom bind(String symbol, int value) {
+        NonAtom output = new NonAtom();
+        output.setLeft(new SymbolicAtom(symbol));
+        output.setRight(new IntegerAtom(value));
+        return output;
+    }
+
     private Stack<NonAtom> getStack(NonAtom... atoms) {
         Stack<NonAtom> output = new Stack<NonAtom>();
         for (NonAtom atom : atoms) {
@@ -18,18 +67,12 @@ public class NonAtomTest {
 
     @Test
     public void testAddPairsSingleArgument() throws LispEvaluationException {
-        NonAtom pList = new NonAtom();
-        NonAtom argList = new NonAtom();
-        pList.setLeft(new SymbolicAtom("A"));
-        pList.setRight(SExpression.NIL);
-        argList.setLeft(new IntegerAtom(5));
-        argList.setRight(SExpression.NIL);
+        NonAtom pList = buildList("A");
+        NonAtom argList = buildList(5);
         Stack<NonAtom> aList = new Stack<NonAtom>();
         NonAtom.addPairs(pList, argList, aList);
 
-        NonAtom aToFiveBinding = new NonAtom();
-        aToFiveBinding.setLeft(new SymbolicAtom("A"));
-        aToFiveBinding.setRight(new IntegerAtom(5));
+        NonAtom aToFiveBinding = bind("A", 5);
         Stack<NonAtom> expectedResult = getStack(aToFiveBinding);
 
         assertEquals(expectedResult, aList);
